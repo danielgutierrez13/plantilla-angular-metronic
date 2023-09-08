7d41baf5e831f7014e7a114dfcfbfd81beacc7b0
Daniel <0512018011@alumnos.unp.edu.pe>
2023-09-08 13:19:18 -0500

    feat: agregar libreria amcharts

---
+++ src/app/model/model-amcharts.ts
@@ -0,0 +1,11 @@
+import * as am5 from "@amcharts/amcharts5";
+
+export interface DataMultipleCircle {
+  value: string;
+  category: string;
+}
+
+export interface AmchartsConfig {
+  id: string;
+  chart: am5.Root;
+}
